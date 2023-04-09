25680c6bb49ed134d15073e05e09f40f212065d9
Alvaro8317 <eduardo831_@hotmail.com>
2023-04-09 15:17:53 -0500

    fix: Change cors parameters

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const routerApi = require('./routes/routes');
+// const cors = require('cors');
 const app = express();
+// app.use(cors());
 
 app.use(express.json());
 require('dotenv').config();
 const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-    res.send('Hello world!');
+  res.send('Hello world!');
 });
 routerApi(app);
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
